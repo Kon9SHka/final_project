803d9b436b01f4da00190728a2b6d6900df13e56
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2022-11-28 17:54:11 +0300

    Создаем изначальный массив (first_array) с размерностью n от пользователя (метод GetNumberFromUser)

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//1. создаем изначальный массив с размерностью n с вводом от пользователя
+string[] first_array = new string [GetNumberFromUser("Введите размер массива: ")];
+
+
+//Методы
+int GetNumberFromUser (string message) 
+{
+    while(true) 
+    {
+
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        while(isCorrect)
+            return number;
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
