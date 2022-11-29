3a823cd4dca2fcf7921be28f147804c0ad2db33f
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2022-11-30 01:46:44 +0300

    заполняем новый массив, который будет включать в себя только значения с необходимым кол-вом символов

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿//1. создаем изначальный массив с размерностью n с вводом от пользователя
 string[] first_array = new string [GetNumberFromUser("Введите размер массива: ")];
-Console.WriteLine(first_array.Length);
+
 //2. заполняем массив произвольными данными от пользователя
 FillArray(first_array);
-Console.WriteLine(first_array[0]);
-Console.WriteLine(first_array[1]);
-Console.WriteLine(first_array[2]);
+
+//3. заполняем новый массив, который будет включать в себя только значенияс 3-мя и менее символами
+string [] result_array = FillArrayWithBord(first_array);
+
+
 //Методы
 //Получаем число от пользователя
 int GetNumberFromUser (string message) 
@@ -20,7 +22,7 @@ int GetNumberFromUser (string message)
         Console.WriteLine("Ошибка ввода!");
     }
 }
-
+//Заполняем изначальный массив
 void FillArray (string [] first_array) 
 { 
     int i = 0;
@@ -33,4 +35,34 @@ void FillArray (string [] first_array)
             break;
     }
 
+}
+//Считаем значения с ограничением по кол-ву символов
+string [] FillArrayWithBord(string[] array)
+{
+    //Определяем кол-во символов для границы
+    int max_border_symbol = GetNumberFromUser("Введите количество символов для ограничения: ");
+    string [] array_method = new string [GetMaxBorder(array,max_border_symbol)];
+    int final_place = 0;
+    for (int i = 0; i < array.Length; i=i+1)
+    {
+        if (array[i].Length<=max_border_symbol)
+        {
+            array_method[final_place] = array[i];
+            final_place = final_place+1;
+        }
+    }
+
+    return array_method;
+}
+//Считаем кол-во записей удовлетворяющих условию
+int GetMaxBorder (string [] array, int max_border_symbol) 
+{
+    int max_border = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=max_border_symbol)
+            max_border = max_border + 1;
+    }
+
+    return max_border;
 }
