71837f90b5b2c54716388aa48c5e08bd001c9c1e
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2022-11-30 00:42:42 +0300

    Добавили метод заполнения первичного массива

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿//1. создаем изначальный массив с размерностью n с вводом от пользователя
 string[] first_array = new string [GetNumberFromUser("Введите размер массива: ")];
-
-
+Console.WriteLine(first_array.Length);
+//2. заполняем массив произвольными данными от пользователя
+FillArray(first_array);
+Console.WriteLine(first_array[0]);
+Console.WriteLine(first_array[1]);
+Console.WriteLine(first_array[2]);
 //Методы
+//Получаем число от пользователя
 int GetNumberFromUser (string message) 
 {
     while(true) 
@@ -14,4 +19,18 @@ int GetNumberFromUser (string message)
             return number;
         Console.WriteLine("Ошибка ввода!");
     }
+}
+
+void FillArray (string [] first_array) 
+{ 
+    int i = 0;
+    while (i < first_array.Length) 
+    {
+        Console.WriteLine($"Введите {i+1}-е значение из {first_array.Length}");
+        first_array[i] = Console.ReadLine() ?? "";
+        i=i+1;
+        if (i==first_array.Length)
+            break;
+    }
+
 }
